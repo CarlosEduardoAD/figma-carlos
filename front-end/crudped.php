cc571800f0fd3eb89da1eeebc125a2300e8228d4
karl <88538786+Karl-developer@users.noreply.github.com>
2022-07-05 17:30:18 -0300

    [FEAT] Implementar login

--- front-end/crudped.php
+++ front-end/crudped.php
@@ -39,9 +39,9 @@ if(!empty($_POST['enviar'])){
 
 if($enviar == 'Enviar'){
     $res_insert = "INSERT INTO restaurante (nome_resturante, avaliacao, endereco, tipo, dono_restaurante) VALUES ('$nome', '10', '$endereco', '$tipo', '$dono') ";
-    $resposta_insert = mysqli_query($conn, $res_insert);
+    $resposta_insert = mysqli_query($conn, $res_select);
     header("location: menu.html");
     die()
-} 
+}
 
 ?>
